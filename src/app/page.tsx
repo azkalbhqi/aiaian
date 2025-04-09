44a2f9cc96d9eed21fda8698e51fab1f474a80d3
azkal <azkalbhqi@gmail.com>
2025-04-10 00:47:51 +0700

    Commit: first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { useEffect, useRef, useState } from 'react';
-import { askGemini } from './chat';
+import { useRef, useState } from 'react';
+import { askGemini } from '@/app/chat';
 //tes
 
 export default function Page() {
